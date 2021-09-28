f88344551d525ae7b75d193dc3f335204f68ad89
Bjoorna <marcusbjorna@gmail.com>
2021-09-28 15:40:27 +0200

    fixed link

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/woapp'));
 
 app.get('/*', (req,res) => 
-    res.sendFile('index.html', {root: 'dist/woapp/'}),
+    res.sendFile('index.html', {root: 'dist/picker/'}),
 );
 
 app.listen(process.env.PORT || 8080);
